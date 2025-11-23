94ad5bc9aad08b728c7b9ce6f05c64cbca4a5ec9
Shaurya Narayan <shauryan240701@gmail.com>
2025-11-23 17:21:16 +0530

    Added SSL for render Database

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
@@ -10,6 +9,12 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT,
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
